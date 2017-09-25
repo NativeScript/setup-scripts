92e0389ad7b96008b922210ad49054f32fe3ca25
Emil Tabakov <emil.tabakov@gmail.com>
2017-09-25 11:23:47 +0300

    Redirect setup script to the new repo

--- mac-startup-shell-script.sh
+++ mac-startup-shell-script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/NativeScript/nativescript-cli/production/setup/native-script.rb)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/NativeScript/setup-scripts/master/native-script.rb)"
