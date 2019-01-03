473ee35e647a5c9e63c008ba6b614da6b3aeda79
Dimitar Topuzov <dtopuzov@gmail.com>
2019-01-03 05:26:04 -0800

    feat: add travis.yml
    - Add travis.yml
    - Redirect brew install log to /dev/null (to reduce verbosity, this is also important for travis job)
    - Install Open JDK 8 instead of Oracle JDK 8
    - Reduce verbosity on Android SDK update (progress bar in console breaks travis ci)
    - Fix emulator creation (echo no instead of echo y)

--- native-script.rb
+++ native-script.rb
@@ -96,8 +96,7 @@ def install_environment_variable(name, value)
 end
 
 # Actually installing all other dependencies
-install("Homebrew", "Installing Homebrew...", 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"</dev/null', false, false)
-
+install("Homebrew", "Installing Homebrew...", 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null 2>&1 </dev/null', false, false)
 if !(execute("brew --version", "Homebrew is not installed or not configured properly. Download it from http://brew.sh/, install, set it up and run this script again."))
   exit
 end
@@ -108,40 +107,23 @@ execute("brew tap caskroom/versions", "", false)
 # Install Google Chrome
 install("Google Chrome", "Installing Google Chrome (required to debug NativeScript apps)", "brew cask install google-chrome", false, false);
 
-# Install JDK 1.8
-install("Java SE Development Kit 8", "Installing the Java SE Development Kit 8 ... This might take some time, please, be patient. (You will be prompted for your password)", 'brew cask install java8', false, false)
+# Install Open JDK 8
+install("Open JDK 8", "Installing Open JDK 8 ... This might take some time, please, be patient.", 'brew tap AdoptOpenJDK/openjdk; brew cask install adoptopenjdk8', false, false)
 unless ENV["JAVA_HOME"]
-  puts "Set JAVA_HOME=$(/usr/libexec/java_home -v 1.8)"
-  install_environment_variable("JAVA_HOME", "$(/usr/libexec/java_home -v 1.8)")
+  java_home = "$(/usr/libexec/java_home -v 1.8)"
+  puts "Set JAVA_HOME=#{java_home}"
+  install_environment_variable("JAVA_HOME", java_home)
 end
 
 # Install Android SDK
-install("Android SDK", "Installing Android SDK", 'brew tap caskroom/cask; brew cask install android-sdk', false)
+install("Android SDK", "Installing Android SDK", 'brew cask install android-sdk', false)
 unless ENV["ANDROID_HOME"]
-  require 'pathname'
   android_home = "/usr/local/share/android-sdk"
-  unless Pathname.new(android_home).exist?
-    require 'mkmf'
-    # if there's no such symlink then try to find the `android-sdk` directory through the `android` executable
-    android_executable_environment_path = find_executable('android')
-    if android_executable_environment_path
-      android_home_joined_path = File.join(android_executable_environment_path, "..", "..")
-      android_home = Pathname.new(android_home_joined_path).realpath
-    end
-  end
+  puts "Set ANDROID_HOME=#{android_home}"
   install_environment_variable("ANDROID_HOME", android_home)
+  puts "Set ANDROID_SDK_ROOT=#{android_home}"
+  install_environment_variable("ANDROID_SDK_ROOT", android_home)
 end
-
-# the -p flag is set in order to ensure zero status code even if the directory exists
-execute("mkdir -p ~/.cocoapods", "There was a problem in creating ~/.cocoapods directory")
-# CocoaPods already has a dependency to xcodeproj and also has a dependency to a lower version of activesupport
-# which works with Ruby 2.0 that comes as the macOS default, so these two installations should be in this order.
-# For more information see: https://github.com/CocoaPods/Xcodeproj/pull/393#issuecomment-231055159
-install("CocoaPods", "Installing CocoaPods... This might take some time, please, be patient.", 'gem install cocoapods -V', true)
-install("CocoaPods", "Setup CocoaPods... This might take some time, please, be patient.", 'pod setup', false)
-install("pip", "Installing pip... This might take some time, please, be patient.", 'easy_install pip', true)
-install("six", "Installing 'six' python package... This might take some time, please, be patient.", 'pip install six', false)
-install("xcodeproj", "Installing xcodeproj... This might take some time, please, be patient.", 'gem install xcodeproj -V', true)
 
 puts "Configuring your system for Android development... This might take some time, please, be patient."
 # Note that multiple license acceptances may be required, hence the multiple commands
@@ -149,21 +131,21 @@ puts "Configuring your system for Android development... This might take some ti
 error_msg = "There seem to be some problems with the Android configuration"
 
 sdk_manager = File.join(ENV["ANDROID_HOME"], "tools", "bin", "sdkmanager")
-execute("echo y | #{sdk_manager} \"platform-tools\"", error_msg)
-execute("echo y | #{sdk_manager} \"tools\"", error_msg)
-execute("echo y | #{sdk_manager} \"build-tools;28.0.3\"", error_msg)
-execute("echo y | #{sdk_manager} \"platforms;android-28\"", error_msg)
-execute("echo y | #{sdk_manager} \"extras;android;m2repository\"", error_msg)
-execute("echo y | #{sdk_manager} \"extras;google;m2repository\"", error_msg)
+execute("echo y | #{sdk_manager} \"tools\" | grep -v = || true", error_msg)
+execute("echo y | #{sdk_manager} \"build-tools;28.0.3\" | grep -v = || true", error_msg)
+execute("echo y | #{sdk_manager} \"platform-tools\" | grep -v = || true", error_msg)
+execute("echo y | #{sdk_manager} \"platforms;android-28\" | grep -v = || true", error_msg)
+execute("echo y | #{sdk_manager} \"extras;android;m2repository\" | grep -v = || true", error_msg)
+execute("echo y | #{sdk_manager} \"extras;google;m2repository\" | grep -v = || true", error_msg)
 
 puts "Do you want to install Android emulator system image? (y/n)"
 if $silentMode || gets.chomp.downcase == "y"
   puts "Do you want to install HAXM (Hardware accelerated Android emulator)? (y/n)"
   if $silentMode || gets.chomp.downcase == "y"
-    execute("echo y | #{sdk_manager} \"extras;intel;Hardware_Accelerated_Execution_Manager\"", "Failed to download Intel HAXM.")
+    execute("echo y | #{sdk_manager} \"extras;intel;Hardware_Accelerated_Execution_Manager\" | grep -v = || true", "Failed to download Intel HAXM.")
     haxm_silent_installer = File.join(ENV["ANDROID_HOME"], "extras", "intel", "Hardware_Accelerated_Execution_Manager", "silent_install.sh")
     execute("sudo #{haxm_silent_installer}", "Failed to install Intel HAXM.")
-    execute("echo y | #{sdk_manager} \"system-images;android-28;google_apis;x86\"", "Failed to download Android emulator system image.")
+    execute("echo y | #{sdk_manager} \"system-images;android-28;google_apis;x86\" | grep -v = || true", "Failed to download Android emulator system image.")
   end
 end
 
@@ -171,8 +153,20 @@ puts "Do you want to create Android emulator? (y/n)"
 if $silentMode || gets.chomp.downcase == "y"
   error_msg = "Failed to create Android emulator."
   avd_manager = File.join(ENV["ANDROID_HOME"], "tools", "bin", "avdmanager")
-  execute("echo y | #{avd_manager} create avd -n Emulator-Api28-Google -k  \"system-images;android-28;google_apis;x86\" -b google_apis/x86 -c 265M -f", error_msg)
+  execute("echo no | #{avd_manager} create avd -n Emulator-Api28-Google -k  \"system-images;android-28;google_apis;x86\" -b google_apis/x86 -c 265M -f", error_msg)
 end
+
+# the -p flag is set in order to ensure zero status code even if the directory exists
+execute("mkdir -p ~/.cocoapods", "There was a problem in creating ~/.cocoapods directory")
+# CocoaPods already has a dependency to xcodeproj and also has a dependency to a lower version of activesupport
+# which works with Ruby 2.0 that comes as the macOS default, so these two installations should be in this order.
+# For more information see: https://github.com/CocoaPods/Xcodeproj/pull/393#issuecomment-231055159
+install("CocoaPods", "Installing CocoaPods... This might take some time, please, be patient.", 'gem install cocoapods', true)
+install("CocoaPods", "Setup CocoaPods... This might take some time, please, be patient.", 'pod setup', false)
+install("pip", "Installing pip... This might take some time, please, be patient.", 'easy_install pip', true)
+install("six", "Installing 'six' python package... This might take some time, please, be patient.", 'pip install six', false)
+install("xcodeproj", "Installing xcodeproj... This might take some time, please, be patient.", 'gem install xcodeproj', true)
 
 puts "The ANDROID_HOME and JAVA_HOME environment variables have been added to your .bash_profile/.zprofile"
 puts "Restart the terminal or run `source ~/.bash_profile` to use them."
+
